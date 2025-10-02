5827b371ff4f86bd8b902eba10b75a8d1a9a5bb4
Delio Jaramillo <64032852+Delio090418@users.noreply.github.com>
2025-10-02 21:39:35 +0200

    Add files via upload

---
+++ data_paths.py
@@ -0,0 +1,7 @@
+data_paths={
+        f"mnist":'path',
+        f"cifar10":'path',
+        f"BRAIN":'path',
+        f"pcam":["split_train_x","split_train_y"],
+        f"isic":["Training_Input","Training_GroundTruth"],
+}
